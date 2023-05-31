c0c4c949f13d35f72ed2909012f7a7e3c74aa9e1
Cazzoletti <paoloczz@f84d8990bd6b.ant.amazon.com>
2023-05-31 19:11:25 +0200

    Added Edit match feature

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ def main():
     st.title("Team Management App")
 
     # Create the main navigation menu
-    menu = ["Player Ranking", "Match Registration", "Match History", "Player Registration"]
+    menu = ["Player Ranking", "Match Registration", "Match History", "Edit Match", "Player Registration"]
     choice = st.sidebar.selectbox("Menu", menu)
 
     if choice == "Player Ranking":
@@ -154,6 +154,8 @@ def main():
         register_match()
     elif choice == "Match History":
         match_history()
+    elif choice == "Edit Match":
+        edit_match()
 
 
 def show_player_ranking():
@@ -174,6 +176,7 @@ def match_history():
     # Retrieve player rankings from the database
     c.execute("""
         SELECT 
+            id,
             locale,
             blue_team_att,
             blue_team_def,
@@ -193,6 +196,7 @@ def match_history():
         pd.DataFrame(
             matches,
             columns=[
+                'GAME_ID',
                 'LOCALE',
                 'BLUE_TEAM_ATT',
                 'BLUE_TEAM_DEF',
@@ -213,6 +217,211 @@ def match_history():
         )
     )
 
+    # Delete button
+    # if st.button("Edit"):
+    #     edit_match()
+
+def edit_match():
+    st.title("Edit Match")
+
+    # Fields to edit match
+    game_id = st.text_input("Enter the Game ID to edit:", value=1)
+    match = get_match_info(game_id)
+
+    all_players = get_player_aliases()
+
+    if match:
+        locale = match[1]
+        match_type = match[6]
+        blue_team_att = match[2]
+        blue_team_att_ix = all_players.index(str(blue_team_att))
+        blue_team_def = match[3]
+        blue_team_def_ix = 0
+        if match_type=='2v2':
+            blue_team_def_ix = all_players.index(str(blue_team_def))
+        red_team_att = match[4]
+        red_team_att_ix = all_players.index(str(red_team_att))
+        red_team_def = match[5]
+        red_team_def_ix = 0
+        if match_type == '2v2':
+            red_team_def_ix = all_players.index(str(red_team_def))
+            alias_title = "Attacker alias"
+        else:
+            alias_title = "Alias"
+        blue_score = match[7]
+        red_score = match[8]
+
+
+        locale = st.selectbox("locale", LOCALES, index=LOCALES.index(locale))
+        match_type = st.selectbox("Game Type", ["1v1", "2v2"], index=["1v1", "2v2"].index(match_type))
+        # Missing locale and game type
+
+        col1, col2 = st.columns(2)
+        # Get user input for team aliases
+        with col1:
+            st.write("Red Team:")
+            red_att = st.selectbox(alias_title, all_players, key='red_att', index=red_team_att_ix)
+            if match_type == '2v2':
+                red_def = st.selectbox("Defender alias", all_players, key='red_def', index=red_team_def_ix)
+            else:
+                red_def = None
+            red_score_ = st.number_input("Score", min_value=0, max_value=10, step=1, key='red_score', value=red_score)
+
+        with col2:
+            st.write("Blue Team:")
+            blue_att = st.selectbox(alias_title, all_players, key='blue_att', index=blue_team_att_ix)
+            if match_type == '2v2':
+                blue_def = st.selectbox("Defender alias", all_players, key='blue_def', index=blue_team_def_ix)
+            else:
+                blue_def = None
+            blue_score_ = st.number_input("Score", min_value=0, max_value=10, step=1, key='blue_score', value = blue_score)
+    else:
+        st.write("Game ID not found.")
+
+    # Save button
+    if st.button("Save"):
+        update_match_info(game_id, locale, blue_att, blue_def, red_att, red_def, match_type,
+                          blue_score_, red_score_)
+        recompute_elo()
+
+# Helper functions for database operations
+def get_match_info(game_id):
+    # Retrieve match information from the database based on the game_id
+    c.execute("""
+        SELECT 
+            id,
+            locale,
+            blue_team_att,
+            blue_team_def,
+            red_team_att,
+            red_team_def,
+            match_type,
+            blue_score,
+            red_score
+        FROM matches 
+        WHERE id = ?
+    """, (game_id,))
+    match = c.fetchone()
+    return match
+
+def update_match_info(game_id, locale, blue_team_att, blue_team_def, red_team_att, red_team_def, match_type,blue_score, red_score):
+    if not valid_teams(blue_team_att, blue_team_def, red_team_att, red_team_def):
+        return st.error('Player names must be different')
+
+    if not valid_goals(blue_score, red_score):
+        return st.error('One team must have scored ten goals')
+
+    # Update the match information in the database based on the game_id
+    c.execute("""
+        UPDATE matches 
+        SET 
+            locale = ?,
+            blue_team_att = ?,
+            blue_team_def = ?,
+            red_team_att = ?,
+            red_team_def = ?,
+            match_type = ?,
+            blue_score = ?,
+            red_score = ?
+        WHERE id = ?
+    """, (locale, blue_team_att, blue_team_def, red_team_att, red_team_def, match_type, blue_score, red_score, game_id))
+    conn.commit()
+
+    st.success("Match successfully Edited.")
+
+def recompute_elo():
+    # Recalculate ELO scores for all players based on the new match information
+    all_matches = get_all_matches()
+
+    # Reset all player scores to the original value
+    reset_player_scores()
+
+    for match in all_matches:
+
+        locale = match[1]
+        blue_team_att = match[2]
+        blue_team_def = match[3]
+        red_team_att = match[4]
+        red_team_def = match[5]
+        match_type = match[6]
+        blue_score = match[7]
+        red_score = match[8]
+
+        # Update elo for all players:
+        # Get Elo for each player
+        blue_team_elo = c.execute(
+            "SELECT alias, elo, games_played FROM players WHERE alias IN (?, ?)",
+            [blue_team_att, blue_team_def]
+        ).fetchall()
+        red_team_elo = c.execute(
+            "SELECT alias, elo, games_played FROM players WHERE alias IN (?, ?)",
+            [red_team_att, red_team_def]
+        ).fetchall()
+
+        blue_team_new_elo, red_team_new_elo = calculate_elo_rating(
+            [_[1] for _ in blue_team_elo],
+            [_[1] for _ in red_team_elo],
+            1 * (blue_score == 10),
+            k_factor=40
+        )
+
+        for i, (player_, _, games_played_) in enumerate(blue_team_elo):
+            c.execute(
+                """
+                UPDATE players
+                SET elo = ?, games_played = ?
+                WHERE alias = ?;
+                """,
+                [blue_team_new_elo[i], games_played_ + 1, player_]
+            )
+            conn.commit()
+
+        for i, (player_, _, games_played_) in enumerate(red_team_elo):
+            c.execute(
+                """
+                UPDATE players
+                SET elo = ?, games_played = ?
+                WHERE alias = ?;
+                """,
+                [red_team_new_elo[i], games_played_ + 1, player_]
+            )
+            conn.commit()
+
+    st.write("ELO scores recomputed.")
+
+def reset_player_scores():
+    all_players = get_player_aliases()
+
+    for p in all_players:
+        # Reset all player scores to the original value
+        c.execute(
+            """
+            UPDATE players
+            SET elo = ?, games_played = ?
+            WHERE alias = ?;
+            """,
+            [1000, 0, p]
+        )
+        conn.commit()
+
+def get_all_matches():
+    # Retrieve all matches from the database
+    c.execute("""
+        SELECT 
+            id,
+            locale,
+            blue_team_att,
+            blue_team_def,
+            red_team_att,
+            red_team_def,
+            match_type,
+            blue_score,
+            red_score
+        FROM matches 
+        ORDER BY timestamp ASC
+    """)
+    matches = c.fetchall()
+    return matches
 
 def register_player():
     st.title("Player Registration")
